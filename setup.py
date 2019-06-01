e2f190ec1a4b16724ae81b6d7a897096a1331e33
Yue Zhihan <zhihan.yue@foxmail.com>
2019-06-01 23:06:51 +0800

    initialize project env

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+def _read(file):
+    with open(file, 'rb') as fp:
+        return fp.read()
+
+setup(name='geyser',
+      version='0.1',
+      description='geyser',
+      long_description=_read('README.md').decode('utf-8'),
+      author='Zhihan Yue',
+      author_email='zhihan.yue@foxmail.com',
+      url='https://github.com/yuezhihan/geyser',
+      license='MIT',
+      python_requires=">=2.7",
+      install_requires=["torch"],
+      packages=["geyser"],
+      zip_safe=False)
